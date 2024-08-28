7ea7c74254deb60f08f412f09a2f790ff6648a5a
RizkinaFatikhatusS <165800759+RizkinaFatkhatusS@users.noreply.github.com>
2024-08-28 14:21:57 +0700

    Add files via upload

---
+++ LatihanTugasPraktikum1/src/latihantugaspraktikum1/DemoNilai.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihantugaspraktikum1;
+
+/**
+ *
+ * @author user
+ */
+public class DemoNilai {
+    public static void main(String [] args) {
+        Nilai nilai = new Nilai("2310631170150", "Rizkina Fatikhatus Sadin", 80, 85, 78, 90);
+        nilai.cetakNilai();
+    }
+}
---
+++ LatihanTugasPraktikum1/src/latihantugaspraktikum1/Mahasiswa.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package latihantugaspraktikum1;
+
+/**
+ *
+ * @author user
+ */
+public class Mahasiswa {
+    void membaca(){
+      System.out.println("Mahasiswa sedang membaca.");
+    }
+    
+    void nyontek(){
+        System.out.println("Mahasiswa sedang menyontek  saat ujian.");
+    }
+    
+    void modifikasi(){
+        System.out.println("Mahasiswa sedang memodifiksi tugas.");
+    }
+}
